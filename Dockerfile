4f8f78d97752089c18562f97e41b355c3cf4e322
sfilata <shubaiqiao@126.com>
2018-11-14 11:36:23 +0800

    add some docker related

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:8.11.3
+MAINTAINER sfilata
+
+ADD . /app/
+WORKDIR /app
+
+RUN npm install
+
+ENV HOST 127.0.0.1
+ENV PORT 8000
+
+EXPOSE 8000
+
+CMD ["npm", "start"]
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 const router = require('koa-router')()
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-const db = mongoose.connect('mongodb://localhost/testDB')
+// const db = mongoose.connect('mongodb://localhost/testDB')
 
-let UserSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    email: String
-})
+// let UserSchema = new mongoose.Schema({
+//     username: String,
+//     password: String,
+//     email: String
+// })
 
-let User = mongoose.model('User', UserSchema)
+// let User = mongoose.model('User', UserSchema)
 
 // let user = {
 //     username: 'sfilata',
@@ -21,7 +21,7 @@ let User = mongoose.model('User', UserSchema)
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
+    title: 'Hello Sfilta!'
   })
 })
 
@@ -45,15 +45,15 @@ router.get('/json', async (ctx, next) => {
 })
 
 router.get('/test', async (ctx, next) => {
-    let val = null
-    const data = await User.findOne({username: 'sfilata'})
-    const result = {
-        code: 200,
-        response: data,
-        ts: 12345
-    }
-    ctx.response.body = result
-    return result
+    // let val = null
+    // const data = await User.findOne({username: 'sfilata'})
+    // const result = {
+    //     code: 200,
+    //     response: data,
+    //     ts: 12345
+    // }
+    // ctx.response.body = result
+    // return result
 })
 
 module.exports = router
